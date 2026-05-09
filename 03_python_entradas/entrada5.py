15ddaf5edacdb57f9f8d4814943de28028a0be24
rapha646 <raphaeldasilva646@gmail.com>
2026-05-09 12:00:48 -0300

    calculo e imc

---
+++ 03_python_entradas/entrada5.py
@@ -0,0 +1,17 @@
+# Autor:Rapha
+# Projeto: entradas com nomes
+
+# variáveis
+nome = input('digite seu nome: ')
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+soma = valor1 + valor2
+multiplicação = valor1 * valor2
+subtração = valor1 - valor2
+divisão = valor1 / valor2
+
+print(nome)
+print(f'A soma dos numeros é: {soma}')
+print(f'A multiplicação dos numeros é: {multiplicação}')
+print(f'A subtração dos numeros é: {subtração}')
+print(f'A divisão dos numeros é: {divisão}')
